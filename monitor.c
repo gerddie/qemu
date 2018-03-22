0924b4ef39be4690785fe4e3edd9af8ab0338df3
Marc-André Lureau <marcandre.lureau@redhat.com>
2018-03-22 16:20:33 +0100

    QmpSession: introduce QmpReturn
    QmpReturn (and associated functions) is used during synchronous
    dispatch return for now. It helps to factor out some code for
    handling a reply context.
    
    In the following patches, the QmpReturn will be the basis upon which
    asynchronous reply will be handled: it will hold the context for a QMP
    command reply.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- monitor.c
+++ monitor.c
@@ -4112,7 +4112,6 @@ static QMPRequest *monitor_qmp_requests_pop_any(void)
 static void monitor_qmp_bh_dispatcher(void *data)
 {
     QMPRequest *req_obj = monitor_qmp_requests_pop_any();
-    QDict *rsp;
     bool need_resume;
 
     if (!req_obj) {
@@ -4127,11 +4126,10 @@ static void monitor_qmp_bh_dispatcher(void *data)
         trace_monitor_qmp_cmd_in_band(qobject_get_try_str(id) ?: "");
         monitor_qmp_dispatch(req_obj->mon, req_obj->req);
     } else {
+        QmpSession *session = &req_obj->mon->qmp.session;
         assert(req_obj->err);
-        rsp = qmp_error_response(req_obj->err);
+        qmp_return_error(qmp_return_new(session, req_obj->req), req_obj->err);
         req_obj->err = NULL;
-        qmp_send_response(req_obj->mon, rsp);
-        qobject_unref(rsp);
     }
 
     if (need_resume) {
